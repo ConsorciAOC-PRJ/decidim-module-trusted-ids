d538c5083fde1f179df2b1f993dc427d9e4468de
Ivan Vergés <ivan@pokecode.net>
2023-10-17 19:44:36 +0200

    ensure proper email texts

--- app/events/decidim/trusted_ids/verifications/invalid_notification.rb
+++ app/events/decidim/trusted_ids/verifications/invalid_notification.rb
@@ -4,19 +4,6 @@ module Decidim
   module TrustedIds
     module Verifications
       class InvalidNotification < SuccessNotification
-        i18n_attributes :handler_name
-
-        def resource_path
-          "http"
-        end
-
-        def resource_url
-          "http"
-        end
-
-        def handler_name
-          I18n.t("decidim.authorization_handlers.trusted_ids_handler.name")
-        end
       end
     end
   end
